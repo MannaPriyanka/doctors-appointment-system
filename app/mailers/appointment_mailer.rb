d7a0c233405f3f49a9c0e3bd8f9f03c8c8969959
PriyankaManna <mannapriyanka04@gmail.com>
2024-11-23 16:01:13 +0530

    Add reminder template

--- app/mailers/appointment_mailer.rb
+++ app/mailers/appointment_mailer.rb
@@ -1,6 +1,9 @@
 class AppointmentMailer < ApplicationMailer
   def reminder_email(appointment)
     @appointment = appointment
+    @doctor = appointment.doctor
+    @patient = appointment.patient
+
     mail(to: @appointment.patient.email, subject: 'Appointment Reminder')
   end
 end
